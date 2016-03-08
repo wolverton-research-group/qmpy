e17dc0851a27ea6f22beca5a7357e0e908376cdc
Logan Ward <ward.logan.t@gmail.com>
2016-03-07 18:10:08 -0600

    Made some changes to the DOS test
    Still not sure where the difference is coming from

--- qmpy/analysis/vasp/test/dos_test.py
+++ qmpy/analysis/vasp/test/dos_test.py
@@ -1,4 +1,5 @@
 from qmpy import DOS, io
+from numpy import mean
 
 strc = io.read('../files/relaxation/POSCAR')
 dos = DOS.read('../files/relaxation/DOSCAR')
@@ -6,12 +7,10 @@ dos = DOS.read('../files/relaxation/DOSCAR')
 # Get the total DOS
 print "Getting total DOS..."
 total_dos = dos.dos
-#print total_dos
 my_dos = dos.get_projected_dos(strc, 'Mg', debug=True)
-my_dos = my_dos / 2
-#print my_dos
-error = max([ abs(x) for x in (total_dos - my_dos)])
-print error
+my_dos = my_dos * 2 * (dos.energy[1] - dos.energy[0])
+error = mean([ abs(x) for x in (total_dos - my_dos)])
+print "\tMean difference between total DOS and projected: ", error
 
 # Get only d orbitals
 print "Getting d orbital DOS..."
