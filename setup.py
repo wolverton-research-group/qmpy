54535fdc40db3c76eba3f60cc9508f6a158a0cca
Vinay Hegde <hegdevinayi@gmail.com>
2018-05-02 23:07:10 -0500

    Version bump to indicate post-release fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qmpy',
-    version='1.1.0',
+    version='1.1.0.post1',
     author='S. Kirklin',
     author_email='scott.kirklin@gmail.com',
     license='LICENSE.txt',
