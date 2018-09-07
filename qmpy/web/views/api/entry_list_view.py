dd38b8d0c9cdd1f6bd1b953b72f3ef18273e8b14
mohanliu <mohan@u.northwestern.edu>
2018-09-06 21:08:14 -0500

    Add entry filters
    Able to filter based on:
    1. composition space
    2. composition that include some elements
    3. composition that exclude some elements

--- qmpy/web/views/api/entry_list_view.py
+++ qmpy/web/views/api/entry_list_view.py
@@ -2,19 +2,45 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from qmpy.web.serializers.entry import EntrySerializer
-from qmpy.materials.entry import Entry
+from qmpy.materials.entry import Entry, Composition
 from api_perm import *
 
 class EntryList(APIView):
-    permission_classes = (OnlyAPIPermission, )
+    #permission_classes = (OnlyAPIPermission, )
     def get(self, request, format=None):
-        entries = Entry.objects.all()[:10]
-        serializer = EntrySerializer(entries, many=True)
+        self.entries = Entry.objects.all()
+        self.request = request
+        self.composition_filter()
+
+        serializer = EntrySerializer(self.entries, many=True)
         return Response(serializer.data)
 
-#@api_view(['GET'])
-#def get_entry_list(request):
-#    if request.method == 'GET':
-#        entries = Entry.objects.all()[:10]
-#        serializer = EntrySerializer(entries, many=True)
-#        return Response(serializer.data)
+    def composition_filter(self):
+        request = self.request
+        entries = self.entries
+
+        comp = request.GET.get('composition', False)
+        if comp:
+            c_lst = comp.strip().split('-')
+            cs = Composition.get_list(c_lst, calculated=True)
+            if len(cs) == 1:
+                c = cs[0]
+            else:
+                c = cs
+            entries = entries.filter(composition=c)
+
+        comp_in = request.GET.get('composition_include', False)
+        if comp_in:
+            cin_lst = Composition.get(comp_in).comp.keys()
+            while cin_lst:
+                tmp_in = cin_lst.pop()
+                entries = entries.filter(composition__element_set=tmp_in)
+
+        comp_ex = request.GET.get('composition_exclude', False)
+        if comp_ex:
+            cex_lst = Composition.get(comp_ex).comp.keys()
+            while cex_lst:
+                tmp_ex = cex_lst.pop()
+                entries = entries.exclude(composition__element_set=tmp_ex)
+
+        self.entries = entries
