6cd6663a77052023649b13b01b76560038c9fa07
mohanliu <mohan@u.northwestern.edu>
2018-09-11 22:54:36 -0500

    Now able to filter and show only the converged calculation info

--- qmpy/web/serializers/entry.py
+++ qmpy/web/serializers/entry.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from qmpy.materials.entry import Entry
+from qmpy.analysis.vasp import Calculation
 from calculation import CalculationSerializer
 
 class EntrySerializer(serializers.ModelSerializer):
-    calculations = CalculationSerializer(source='calculation_set', many=True, read_only=True)
+    calculations = serializers.SerializerMethodField('get_calc')
+
+    def get_calc(self, entry):
+        qs = Calculation.objects.filter(converged=True, 
+                                        label__in=['static', 'hse06'], 
+                                        entry=entry)
+        serializer = CalculationSerializer(instance=qs, many=True)
+        return serializer.data
 
     class Meta:
         model = Entry
-        fields = ('id', 'path', 'natoms', 'energy', 'calculations')
+        fields = ('id', 'name', 'composition', 'natoms', 'energy', 'calculations')
