37f9ceeee6f0009da45a3234924df2af3d697cba
mohanliu <mohan@u.northwestern.edu>
2018-10-09 20:57:43 -0500

    Add formation_energy information to entry list view

--- qmpy/web/serializers/entry.py
+++ qmpy/web/serializers/entry.py
@@ -3,16 +3,19 @@ from django.db.models import Q
 
 from qmpy.materials.entry import Entry
 from qmpy.materials.structure import Structure
+from qmpy.materials.formation_energy import FormationEnergy
 from qmpy.analysis.vasp import Calculation
 
 from calculation import CalculationSerializer
 from composition import CompositionSerializer
 from structure import StructureSerializer
+from formationenergy import FormationEnergySerializer
 
 class EntrySerializer(serializers.ModelSerializer):
     composition = serializers.SerializerMethodField('get_comp')
     calculations = serializers.SerializerMethodField('get_calc')
     structures = serializers.SerializerMethodField('get_strct')
+    formationenergies = serializers.SerializerMethodField('get_fe')
 
     def get_comp(self, entry):
         serializer = CompositionSerializer(instance=entry.composition)
@@ -31,7 +34,13 @@ class EntrySerializer(serializers.ModelSerializer):
         serializer = StructureSerializer(instance=sts, many=True)
         return serializer.data
 
+    def get_fe(self, entry):
+        fes = FormationEnergy.objects.filter(entry=entry)
+        serializer = FormationEnergySerializer(instance=fes, many=True)
+        return serializer.data
+
     class Meta:
         model = Entry
-        fields = ('id', 'name', 'composition', 'prototype', 'ntypes', 'natoms', 'energy',
-                  'structures', 'calculations')
+        fields = ('id', 'name', 'path', 'composition', 'prototype', 
+                  'ntypes', 'natoms', 'energy', 'keywords', 'holds',
+                  'structures', 'calculations', 'formationenergies')
