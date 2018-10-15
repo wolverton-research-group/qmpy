de04c8090b55d9db18a0285f4e0baddec5a18263
mohanliu <mohan@u.northwestern.edu>
2018-10-15 11:26:07 -0500

    Update qmpy_rester
    1. get_entries by keywords:
        1. composition
        2. composition_include
        3. calculated
        4. gap
        5. ntypes
        6. generic
    2. get_calculations by keywords:
        1. converged
        2. label
        3. gap
    3. get single entry/calculation by id

--- qmpy/rester/qmpy_rester.py
+++ qmpy/rester/qmpy_rester.py
@@ -23,5 +23,31 @@ class QMPYRester(object):
                 data = json.loads(response.text)
                 return data
 
-    def get_entries_from_composition(self, comp):
-        return self._make_requests('/entry?composition=%s'%comp)
+    def get_entries(self, **kwargs):
+        url_args = []
+        kwargs_list = ['composition', 'composition_include', 'calculated',
+                       'gap', 'ntypes', 'generic']
+
+        for k in kwargs_list:
+            if k in kwargs:
+                url_args.append('%s=%s' %(k, kwargs[k]))
+
+        _url = '&'.join(url_args)
+        return self._make_requests('/entry?%s'%_url)
+
+    def get_entry_by_id(self, entry_id):
+        return self._make_requests('/entry/%d'%entry_id)
+
+    def get_calculations(self, **kwargs):
+        url_args = []
+        kwargs_list = ['converged', 'label', 'gap']
+
+        for k in kwargs_list:
+            if k in kwargs:
+                url_args.append('%s=%s' %(k, kwargs[k]))
+
+        _url = '&'.join(url_args)
+        return self._make_requests('/calculation?%s'%_url)
+
+    def get_calculation_by_id(self, calc_id):
+        return self._make_requests('/calculation/%d'%calc_id)
