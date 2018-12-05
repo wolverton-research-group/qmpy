24795d223df787021be084487c20a5ef6dacbf6f
Vinay Hegde <hegdevinayi@gmail.com>
2018-12-05 15:03:30 -0600

    Add docstring to CSLD INI I/O functions

--- qmpy/io/ini.py
+++ qmpy/io/ini.py
@@ -14,6 +14,41 @@ class INIError(Exception):
 
 
 def read(filename=None):
+    """
+    Converts sections/options in INI files into a Python dictionary.
+
+    (Note: Python3 provides this functionality already with the
+    `ConfigParser.read()` function. This is mostly for compatibility between
+    Python 2 and 3.)
+
+    Args:
+        filename:
+            String with the path to the CSLD INI file to read from
+
+    Returns:
+        Two-level dictionary with the following structure:
+        {
+            "section1": {
+                "option1": "value1",
+                "option2": "value2",
+                ...
+            },
+            "section2": {
+                "option1": "value1",
+                "option2": "value2",
+                ...
+            },
+            ...
+        }
+
+        NOTE #1: No type-conversion is performed for parsed values. They are
+        returned as strings, similar to the proxy-dictionary returned by
+        Python3's `ConfigParser.read()` function.
+
+        NOTE #2: Empty/None values in the CSLD INI file are skipped entirely
+        but empty sections are retained in the resulting dictionary.
+
+    """
     if filename is None:
         return {}
     config = configparser.ConfigParser()
@@ -30,6 +65,23 @@ def read(filename=None):
 
 
 def write(data_dict=None, filename=None):
+    """
+    Writes dictionary data to file in the .ini/.cfg format.
+
+    Args:
+        data_dict:
+            Dictionary to be written to file. By design, only two levels of
+            the dictionary are written to file and rest of the dictionary is
+            ignored.
+
+        filename:
+            String with the full path to the file into which the dictionary
+            should be written.
+
+    Returns:
+        None.
+
+    """
     if not data_dict:
         return
     if not filename:
