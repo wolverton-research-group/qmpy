0f5b16ecf6ec0de97e180e3d7fc2df9c8114f0f7
Vinay Hegde <hegdevinayi@gmail.com>
2018-12-16 20:42:33 -0600

    Refactor `django.core.context_processors` -> `django.template.context_processors`

--- qmpy/web/views/materials/composition.py
+++ qmpy/web/views/materials/composition.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.core.context_processors import csrf
+from django.template.context_processors import csrf
 
 from qmpy import INSTALL_PATH
 from ..tools import get_globals
