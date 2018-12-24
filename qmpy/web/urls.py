a4869f0315442f20b9e312f6a4f868b3deddd5e3
Vinay Hegde <hegdevinayi@gmail.com>
2018-12-24 03:14:48 -0600

    Fix typos in urls.py, remove 'include' as per convention

--- qmpy/web/urls.py
+++ qmpy/web/urls.py
@@ -1,23 +1,34 @@
-from django.conf.urls import include, url
+"""qmpy URL Configuration
 
-import django.views.generic
-from django.views.generic import DetailView, ListView
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
 
 import qmpy.web.views as views
-from qmpy.db import settings
 
-# Uncomment the next two lines to enable the admin:
-admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.home_page),
 
     ## admin
+    # TODO: remove code related to admin interface from qmpy
+    url(r'^admin/', admin.site.urls)
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^accounts/login/.*$', django.contrib.auth.login),
     url(r'^accounts/logout/.*$', django.contrib.auth.logout),
-    url(r'^admin/', include(admin.site.urls))
     ]
 
 urlpatterns += [
