9c686d1148981c2dfb1b74db86ed88d136997fc3
mohanliu <mohan@u.northwestern.edu>
2019-05-01 15:08:45 -0500

    Update function to filter attributes of FormationEnergy

--- qmpy/utils/expression_evaluation.py
+++ qmpy/utils/expression_evaluation.py
@@ -1,5 +1,6 @@
 from django.db.models import Q 
 import operator
+import re
 import qmpy.data as data
 
 def precedence(op):
@@ -11,14 +12,83 @@ def precedence(op):
         return 0
 
 def applyOP(q1, q2, op):
-    if op == '-':
+    if op == '&' or op == '-':
         return reduce(operator.and_, [q1, q2])
-    elif op == ',':
+    elif op == '|' or op == ',':
         return reduce(operator.or_, [q1, q2])
 
-class Token:
+class Token(object):
     def __init__(self, tokens):
         self.tokens = tokens
+
+    def filter_formationenergy(self, expr):
+        if type(expr) == Q:
+            return expr
+        else:
+            attr = re.split("[=<>]", expr)[0]
+            val = re.split("[=<>]", expr)[1]
+
+            if attr == "element":
+                return Q(composition__element_list__contains=val+'_')
+
+            elif attr == "generic":
+                return Q(composition__generic=val)
+
+            elif attr == "prototype":
+                return Q(entry__prototype=val)
+
+            elif attr == "spacegroup":
+                return Q(calculation__output__spacegroup__hm=val)
+
+            elif attr == "volume":
+                if '>' in expr:
+                    return Q(calculation__output__volume__gt=val)
+                elif '<' in expr:
+                    return Q(calculation__output__volume__lt=val)
+                elif '=' in expr:
+                    return Q(calculation__output__volume=val)
+
+            elif attr == "natoms":
+                if '>' in expr:
+                    return Q(entry__natoms__gt=val)
+                elif '<' in expr:
+                    return Q(entry__natoms__lt=val)
+                elif '=' in expr:
+                    return Q(entry__natoms=val)
+
+            elif attr == "ntypes":
+                if '>' in expr:
+                    return Q(entry__ntypes__gt=val)
+                elif '<' in expr:
+                    return Q(entry__ntypes__lt=val)
+                elif '=' in expr:
+                    return Q(entry__ntypes=val)
+
+            elif attr == "stability":
+                if '>' in expr:
+                    return Q(stability__gt=val)
+                elif '<' in expr:
+                    return Q(stability__lt=val)
+                elif '=' in expr:
+                    return Q(stability=val)
+
+            elif attr == "delta_e":
+                if '>' in expr:
+                    return Q(delta_e__gt=val)
+                elif '<' in expr:
+                    return Q(delta_e__lt=val)
+                elif '=' in expr:
+                    return Q(delta_e=val)
+
+            elif attr == "band_gap":
+                if '>' in expr:
+                    return Q(calculation__band_gap__gt=val)
+                elif '<' in expr:
+                    return Q(calculation__band_gap__lt=val)
+                elif '=' in expr:
+                    return Q(calculation__band_gap=val)
+
+            return Q()
 
     def element2q(self, val):
         if type(val) == Q:
@@ -30,6 +100,79 @@ class Token:
         lst = data.element_groups[group]
         q_lst = [Q(composition__element_list__contains=v+'_') for v in lst]
         return reduce(operator.or_, q_lst)
+
+    def evaluate_filter(self, origin='formationenergy'):
+        value_stack = []
+        operator_stack = []
+
+        tokens = self.tokens
+        it = 0
+        while it < len(tokens):
+            t = tokens[it]
+
+            if t == ' ':
+                it += 1
+                continue
+
+            elif t.isalpha():
+                expr = ''
+                while t != ' ' and it < len(tokens):
+                    expr += t
+                    it += 1
+                    if it < len(tokens):
+                        t = tokens[it]
+                value_stack.append(expr)
+    
+            elif t == '(':
+                operator_stack.append(t)
+                it += 1
+    
+            elif t == ')':
+                while operator_stack[-1] != '(':
+                    op = operator_stack.pop()
+    
+                    q2 = getattr(self, 'filter_'+origin)(value_stack.pop())
+                    q1 = getattr(self, 'filter_'+origin)(value_stack.pop())
+    
+                    val = applyOP(q1, q2, op)
+    
+                    value_stack.append(val)
+    
+                operator_stack.pop()
+                it += 1
+
+            elif t in ['&', '|']:
+                thisop = t
+                while operator_stack:
+                    if not precedence(operator_stack[-1]) < precedence(thisop):
+                        op = operator_stack.pop()
+    
+                        q2 = getattr(self, 'filter_'+origin)(value_stack.pop())
+                        q1 = getattr(self, 'filter_'+origin)(value_stack.pop())
+    
+                        val = applyOP(q1, q2, op)
+    
+                        value_stack.append(val)
+                    else:
+                        break
+    
+                operator_stack.append(thisop)
+                it += 1
+    
+            else:
+                raise
+    
+        while operator_stack:
+            op = operator_stack.pop()
+
+            q2 = getattr(self, 'filter_'+origin)(value_stack.pop())
+            q1 = getattr(self, 'filter_'+origin)(value_stack.pop())
+            
+            val = applyOP(q1, q2, op)
+            
+            value_stack.append(val)
+    
+        return getattr(self, 'filter_'+origin)(value_stack.pop())
 
     def evaluate(self, method='element2q'):
         value_stack = []
