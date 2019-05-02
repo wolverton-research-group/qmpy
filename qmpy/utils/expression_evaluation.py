ad7731b03dfdc1a62f4842e795dbce0cad6e189c
mohanliu <mohan@u.northwestern.edu>
2019-05-01 19:10:49 -0500

    Deprecate old function of composition filter

--- qmpy/utils/expression_evaluation.py
+++ qmpy/utils/expression_evaluation.py
@@ -4,17 +4,17 @@ import re
 import qmpy.data as data
 
 def precedence(op):
-    if op == '-':
+    if op == '&':
         return 2
-    elif op == ',':
+    elif op == '|':
         return 1
     else:
         return 0
 
 def applyOP(q1, q2, op):
-    if op == '&' or op == '-':
+    if op == '&':
         return reduce(operator.and_, [q1, q2])
-    elif op == '|' or op == ',':
+    elif op == '|':
         return reduce(operator.or_, [q1, q2])
 
 class Token(object):
@@ -101,16 +101,10 @@ class Token(object):
 
             return Q()
 
-    def element2q(self, val):
-        if type(val) == Q:
-            return val
-        else:
-            return Q(composition__element_list__contains=val+'_')
-
-    def parse_element_group(self, group):
-        lst = data.element_groups[group]
-        q_lst = [Q(composition__element_list__contains=v+'_') for v in lst]
-        return reduce(operator.or_, q_lst)
+#    def parse_element_group(self, group):
+#        lst = data.element_groups[group]
+#        q_lst = [Q(composition__element_list__contains=v+'_') for v in lst]
+#        return reduce(operator.or_, q_lst)
 
     def evaluate_filter(self, origin='formationenergy'):
         """
@@ -191,89 +185,3 @@ class Token(object):
             value_stack.append(val)
     
         return getattr(self, 'filter_'+origin)(value_stack.pop())
-
-    def evaluate(self, method='element2q'):
-        value_stack = []
-        operator_stack = []
-
-        tokens = self.tokens
-        it = 0
-        while it < len(tokens):
-            t = tokens[it]
-
-            if t == ' ':
-                it += 1
-                continue
-
-            elif t.isupper():
-                value_stack.append(t)
-                it += 1
-    
-            elif t.islower():
-                ele = value_stack.pop()
-                ele += t
-                value_stack.append(ele)
-                it += 1
-    
-            elif t == '(':
-                operator_stack.append(t)
-                it += 1
-    
-            elif t == ')':
-                while operator_stack[-1] != '(':
-                    op = operator_stack.pop()
-    
-                    q2 = getattr(self, method)(value_stack.pop())
-                    q1 = getattr(self, method)(value_stack.pop())
-    
-                    val = applyOP(q1, q2, op)
-    
-                    value_stack.append(val)
-    
-                operator_stack.pop()
-                it += 1
-
-            elif t == '{':
-                it += 1
-                t = tokens[it]
-                group = ''
-                while t != '}':
-                    group += t
-                    it += 1
-                    t = tokens[it]
-                it += 1
-                value_stack.append(self.parse_element_group(group))
-                continue
-    
-            elif t in ['-', ',']:
-                thisop = t
-                while operator_stack:
-                    if not precedence(operator_stack[-1]) < precedence(thisop):
-                        op = operator_stack.pop()
-    
-                        q2 = getattr(self, method)(value_stack.pop())
-                        q1 = getattr(self, method)(value_stack.pop())
-    
-                        val = applyOP(q1, q2, op)
-    
-                        value_stack.append(val)
-                    else:
-                        break
-    
-                operator_stack.append(thisop)
-                it += 1
-    
-            else:
-                raise
-    
-        while operator_stack:
-            op = operator_stack.pop()
-
-            q2 = getattr(self, method)(value_stack.pop())
-            q1 = getattr(self, method)(value_stack.pop())
-            
-            val = applyOP(q1, q2, op)
-            
-            value_stack.append(val)
-    
-        return getattr(self, method)(value_stack.pop())
