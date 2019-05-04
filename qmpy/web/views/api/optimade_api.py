57da08ba1f3867278ae848911a4bb3a30a642acf
mohanliu <mohan@u.northwestern.edu>
2019-05-04 05:20:28 -0500

    Optimade API for structures data in OQMD

--- qmpy/web/views/api/optimade_api.py
+++ qmpy/web/views/api/optimade_api.py
@@ -4,17 +4,54 @@ from qmpy.web.serializers.optimade import OptimadeStructureSerializer
 from qmpy.materials.formation_energy import FormationEnergy
 from qmpy.materials.entry import Composition
 from qmpy.utils import Token, parse_formula_regex
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
-from rest_framework.response import Response
 
-class OptimadePagination(PageNumberPagination):
-#class OptimadePagination(LimitOffsetPagination):
-    def get_paginated_response(self, data):
-        return Response({
-            'links': self.get_next_link(),
-            'count': self.page.paginator.count,
-            'results': data,
-        })
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+
+from collections import OrderedDict
+
+import time
+import datetime
+
+BASE_URL = "http://larue.northwestern.edu:9000/optimade"
+
+class OptimadePagination(LimitOffsetPagination):
+    def get_paginated_response(self, page_data):
+        data = page_data["data"]
+        request = page_data["request"]
+
+        full_url = request.build_absolute_uri()
+        representation = full_url.replace(BASE_URL, '')
+
+        time_now = time.time()
+        time_stamp = datetime.datetime.fromtimestamp(time_now).strftime(
+            '%Y-%m-%d %H:%M:%S'
+        )
+
+        return Response(OrderedDict([
+            ('links', 
+             OrderedDict([('next', self.get_next_link()),
+              ('base_url', {
+                  "href": BASE_URL,
+                  "meta":{'_oqmd_version': "1.0"}
+              })
+             ])
+            ),
+            ('resource', {}),
+            ('data', data),
+            ('meta', 
+             OrderedDict([
+                 ("query", {"representation": representation}),
+                 ("api_version", "1.0"),
+                 ("time_stamp", time_stamp), 
+                 ("data_returned", min(self.get_limit(request), self.count)),
+                 ("data_available", self.count),
+                 ("more_data_available", (self.get_next_link() != None))
+             ])
+            ),
+            ("response_message", "OK")
+        ]))
 
 class OptimadeStructureList(generics.ListAPIView):
     serializer_class = OptimadeStructureSerializer
@@ -22,7 +59,6 @@ class OptimadeStructureList(generics.ListAPIView):
 
     def get_queryset(self):
         fes = FormationEnergy.objects.filter(fit="standard")
-        fes = self.composition_filter(fes)
         fes = self.filter(fes)
 
         return fes
@@ -33,78 +69,16 @@ class OptimadeStructureList(generics.ListAPIView):
 
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+            page_data = {"data": serializer.data, "request": self.request}
+            return self.get_paginated_response(page_data)
 
         serializer = self.get_serializer(query_set, many=True)
         return Response(serialzer.data)
 
-    def composition_filter(self, fes):
-        """
-        Valid url parameter inputs:
-            1. ?composition=Fe2O3
-            2. ?composition=Fe-O
-            3. ?composition={Fe,Ni}O
-            4. ?composition={3d}2O3
-        """
-        request = self.request
-
-        comp = request.GET.get('composition', False)
-        if not comp:
-            return fes
-
-        if '{' and '}' in comp:
-            c_dict_lst = parse_formula_regex(comp)
-            f_lst = []
-            for cd in c_dict_lst:
-                f = ' '.join(['%s%g' % (k, cd[k]) for k in sorted(cd.keys())])
-                f_lst.append(f)
-            fes = fes.filter(composition__formula__in=f_lst)
-        else:
-            c_lst = comp.strip().split('-')
-            cs = Composition.get_list(c_lst)
-            if len(cs) == 1:
-                c = cs[0]
-            else:
-                c = cs
-            fes = fes.filter(composition=c)
-
-        return fes
-
-
     def filter(self, fes):
-        """
-        Valid attributes:
-            element, generic, prototype, spacegroup,
-            volume, natoms, ntypes, stability,
-            delta_e, band_gap
-
-        Requirments:
-            1. Space padding is required between expression. 
-            2. For each epression, space is not allowed.
-            3. Operators include: 'AND', 'OR'
-            4. '(' and ')' can be used to change precedence
-            5. For numerical attributes, we can have '>' or '<' conditions.
-            Valid examples:
-                'element=Mn AND band_gap>1'
-                '( element=O OR element=S ) AND natoms<3'
-            Invalid examples:
-                'element = Fe'
-                '( element=Fe And element=O)'
-
-        Additionally, instead of multiple 'element' expressions, we can use
-        'element_set' expression to combine elements in the filter.
-
-        Format of element_set expression:
-            '-': AND operator
-            ',': OR operator
-            '(', ')': to change precedence
-            Examples:
-                element_set=Al,O-H
-                element_set=(Mn,Fe)-O
-        """
         request = self.request
 
-        filters = request.GET.get('filters', False)
+        filters = request.GET.get('filter', False)
 
         if not filters:
             return fes
