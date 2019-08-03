e0f0c54d1eff3cd198fc5c559232cbb1505b5ef3
Vinay Hegde <hegdevinayi@gmail.com>
2019-08-03 13:33:21 -0500

    Fix error in calculating volume of replacing atom
    The final volume is calculated now as:
    
    V_fin = V_init - \sum_i V_i/(\sum_j V_j)*V_init + \sum_i' V_i'/(\sum_j' V_j')*V_init

--- qmpy/materials/structure.py
+++ qmpy/materials/structure.py
@@ -1714,17 +1714,18 @@ class Structure(models.Model, object):
             new.substitute(replace, rescale=rescale, in_place=True)
             return new
 
-        volume = self.get_volume()
+        init_vol = self.get_volume()
+	final_vol = init_vol
 	volume_sum_atom = self.get_volume_sum_of_elements()
         for atom in self:
             if atom.element_id in replace:
-                volume -= atom.element.volume*volume/volume_sum_atom
+                final_vol -= atom.element.volume/volume_sum_atom*init_vol
 		volume_sum_atom -= atom.element.volume
                 atom.element = Element.get(replace[atom.element_id])
-                volume += atom.element.volume*volume/volume_sum_atom
 		volume_sum_atom += atom.element.volume
+                final_vol += atom.element.volume/volume_sum_atom*init_vol
         if rescale and rescale_method == "relative":
-            self.set_volume(volume)
+            self.set_volume(final_vol)
         elif rescale and rescale_method == "absolute":
             self.set_volume_to_sum_of_elements()
         self.set_composition()
