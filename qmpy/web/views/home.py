dad1c975f004ff411e65f936cc03f107723b8be7
mohanliu <mohan@u.northwestern.edu>
2019-09-19 14:24:38 -0500

    Display the number of compounds calculated

--- qmpy/web/views/home.py
+++ qmpy/web/views/home.py
@@ -9,11 +9,7 @@ from tools import get_globals
 def home_page(request):
     data = get_globals()
     data.update({
-        'done':600000 #Entry.objects.filter(
-    #        formationenergy__fit='standard').distinct() #,
-    #    'running':Task.objects.filter(state=1),
-    #    'recent':Calculation.objects.filter(label='static',
-    #        converged=True).order_by('-id')[:5]
+        'done': '{:,}'.format(Formation.objects.filter(fit='standard').count()),
                  })
     request.session.set_test_cookie()
     return render_to_response('index.html',
